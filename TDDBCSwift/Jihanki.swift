7bbcd69b3eba8c729a16229b6a1cbd829b402bd2
Hiroya Hinomori <hiroya.hinomori@nhn-comico.com>
2018-09-29 13:36:02 +0900

    ボタンを押すとコーラが出るテストを通した

---
+++ TDDBCSwift/Jihanki.swift
@@ -0,0 +1,15 @@
+//
+//  Jihanki.swift
+//  TDDBCSwift
+//
+//  Created by HINOMORI HIROYA on 2018/09/29.
+//  Copyright © 2018年 tddbc. All rights reserved.
+//
+
+import Foundation
+
+struct Jihanki{
+    func buttonPush() -> String{
+        return "コーラ"
+    }
+}
