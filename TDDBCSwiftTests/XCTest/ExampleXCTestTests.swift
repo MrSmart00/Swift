e9736119e4c649a36df61e34744cfd8c56593bcc
Hiroya Hinomori <hiroya.hinomori@nhn-comico.com>
2018-09-29 13:32:46 +0900

    Hello Jihanki

--- TDDBCSwiftTests/XCTest/ExampleXCTestTests.swift
+++ TDDBCSwiftTests/XCTest/ExampleXCTestTests.swift
@@ -1,9 +1,13 @@
 import XCTest
 @testable import TDDBCSwift
 
+// TODO: [] ボタンを押すとコーラが出る
+
 class ExampleXCTestTests: XCTestCase {
-    func testHelloWorld() {
-        let sample = Sample()
-        XCTAssertEqual(sample.say(), "Hello TDD BootCamp!")
+    
+    func testボタンを押すとコーラが出る() {
+        let jihanki = Jihanki()
+        let result = jihanki.buttonPush()
+        XCTAssert(result == "コーラ")
     }
 }
