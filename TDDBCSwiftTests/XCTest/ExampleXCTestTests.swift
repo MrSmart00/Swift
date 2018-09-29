91fc376a992ccc291a8a3917e518192564b435eb
Hiroya Hinomori <hiroya.hinomori@nhn-comico.com>
2018-09-29 16:08:40 +0900

    テストコードに自販機のcreationメソッドを追加

--- TDDBCSwiftTests/XCTest/ExampleXCTestTests.swift
+++ TDDBCSwiftTests/XCTest/ExampleXCTestTests.swift
@@ -14,66 +14,76 @@ import XCTest
 
 class ExampleXCTestTests: XCTestCase {
     
-    func test100円を入れるとコーラが出る() {
+    func createEnptyJihanki() -> Jihanki{
+        return Jihanki()
+    }
+    
+    func create100YenJihanki() -> Jihanki{
         let jihanki = Jihanki()
+        jihanki.insert(.hundred)
+        return jihanki
+    }
+
+    func create200YenJihanki() -> Jihanki{
+        let jihanki = Jihanki()
+        jihanki.insert(.hundred)
+        jihanki.insert(.hundred)
+        return jihanki
+    }
+
+    func test100円を入れるとコーラが出る() {
+        let jihanki = createEnptyJihanki()
         jihanki.insert(.hundred)
         let result = jihanki.pushButton(drink: .coke)
         XCTAssert(result == "コーラ")
     }
     
     func test100円を入れると烏龍茶が出る() {
-        let jihanki = Jihanki()
-        jihanki.insert(.hundred)
+        let jihanki = create100YenJihanki()
         let result = jihanki.pushButton(drink: .oolong)
         XCTAssert(result == "烏龍茶")
     }
 
     func test200円を入れるとレッドブルが出る() {
-        let jihanki = Jihanki()
-        jihanki.insert(.hundred)
-        jihanki.insert(.hundred)
+        let jihanki = create200YenJihanki()
         let result = jihanki.pushButton(drink: .redbull)
         XCTAssert(result == "レッドブル")
     }
 
     func test100円を入れてもレッドブルが買えない() {
-        let jihanki = Jihanki()
-        jihanki.insert(.hundred)
+        let jihanki = create100YenJihanki()
         let result = jihanki.pushButton(drink: .redbull)
         XCTAssert(result == nil)
     }
     
     func test100円入れてボタン複数回押してもドリンクは一本しか出ない() {
-        let jihanki = Jihanki()
-        jihanki.insert(.hundred)
+        let jihanki = create100YenJihanki()
         let result1 = jihanki.pushButton(drink: .coke)
         let result2 = jihanki.pushButton(drink: .coke)
         XCTAssert(result1 == "コーラ" && result2 == nil)
     }
     
     func test100円入れるとコーラボタンが光る() {
-        let jihanki = Jihanki()
-        jihanki.insert(.hundred)
+        let jihanki = create100YenJihanki()
         let result = jihanki.isEnable(.coke)
         XCTAssert(result)
     }
     
     func test100円入れてもレッドブルボタンが光らない() {
-        let jihanki = Jihanki()
-        jihanki.insert(.hundred)
+        let jihanki = create100YenJihanki()
         let result = jihanki.isEnable(.redbull)
         XCTAssertFalse(result)
     }
 
     func test500円も使えるようになる() {
-        let jihanki = Jihanki()
+        let jihanki = createEnptyJihanki()
         jihanki.insert(.fiveHundred)
         let result = jihanki.pushButton(drink: .coke)
         XCTAssert(result == "コーラ")
     }
     
     func test10円と50円が使えるようになる() {
-        let jihanki = Jihanki()
+        let jihanki = createEnptyJihanki()
         jihanki.insert(.ten)
         jihanki.insert(.ten)
         jihanki.insert(.ten)
